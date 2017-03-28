d1c6daf411a6f9290971f4019914456be5e29294
ThatPeter <peter.malo@centrum.sk>
2017-03-28 22:59:20 +0200

    Update playfield.cpp

--- playfield.cpp
+++ playfield.cpp
@@ -93,7 +93,7 @@
 
             if ((enterCounter++ + 1) % 3 == 0)
                 cout << endl << endl;
-        }
+        } 
  
         cout << "-------------------" << endl;
         cout << endl << endl;
